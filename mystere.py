ad545e07cf67a433a904195fb11b069277f465f6
inesmassa <inesurvivor@gmail.com>
2026-04-01 14:46:47 +0200

    fin mystere

---
+++ mystere.py
@@ -0,0 +1,20 @@
+from random import *
+
+#Le programme choisi un nombre au hasard entre 0 et 123, le nombre mystère.
+
+choix_mystere = choice(range(0, 123, 1))
+print(choix_mystere)
+
+#Il demande à l'utilisateur de le deviner.
+
+#L'utilisateur choisit un nombre.
+#Le programme compare le nombre avec le nombre mystère
+while not False:
+    texte = int(input('devine mon nombre:'))
+    if texte == choix_mystere:
+        print('bravo')
+        break
+    elif texte > choix_mystere:
+        print('c\'est moins')
+    else:
+        print('c\'est plus')
